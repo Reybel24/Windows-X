ee76b834e2c8d4ce9d37cbbe1421fff63897734b
reybel24 <reybel24@gmail.com>
2020-07-20 03:58:28 -0400

    add launch app animation

---
+++ src/views/web/WebsiteCore.vue
@@ -0,0 +1,37 @@
+<script>
+// Common functionality for websites
+import store from '@/store';
+export default {
+  props: {
+    app: {
+      type: Object,
+      default: () => {},
+      required: false
+    },
+    procId: {
+      type: Number,
+      default: -1,
+      required: false
+    },
+    payload: {
+      type: Object,
+      default: null,
+      required: false
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    exitApp() {},
+    maximizeApp() {},
+    minimizeApp() {},
+    setInitialPos(x, y) {}
+  },
+  computed: {
+    coreStyle: function() {
+      return {};
+    }
+  }
+};
+</script>
