b17d87700458cc839978e359327d4e8807f17c16
reybel24 <reybel24@gmail.com>
2020-07-23 03:14:28 -0400

    small ui adjustment to netflix app

--- src/components/apps/AppNetflix.vue
+++ src/components/apps/AppNetflix.vue
@@ -236,7 +236,6 @@ export default {
     flex-direction: column;
 
     .row {
-      height: 160px;
       flex-direction: column;
       margin-bottom: 30px;
     }
@@ -248,7 +247,7 @@ export default {
     }
 
     .items-group {
-      height: 165px;
+      height: 155px;
       // width: 200px;
       flex-direction: row;
       margin-top: 9px;
@@ -266,7 +265,7 @@ export default {
 
       .item {
         width: 230px;
-        height: 85%;
+        height: 83%;
         border-radius: 5px;
         margin: 0 5px 0 5px;
         transition: 2s;
@@ -308,7 +307,7 @@ export default {
 
           .logo {
             width: 50%;
-            height: 41%;
+            height: 36%;
             margin-top: 4%;
             margin-bottom: 5%;
             padding-left: 5%;
